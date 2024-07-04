ce2f120fec2217d3d51f91e06975a59d0dc8ed9e
Alexandre <alexandre.ricard@epitech.eu>
2024-07-04 19:54:27 +0200

    ADD: xcode project

---
+++ ScoreMania/ScoreMania/ScoreManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreManiaApp.swift
+//  ScoreMania
+//
+//  Created by Alexandre Ricard on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScoreManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
